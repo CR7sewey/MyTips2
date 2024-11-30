4af4d7c5c8adfe8b49b324db357c85c347c93785
CR7sewey <miguelmotacarvalho@gmail.com>
2024-11-30 19:02:23 +0000

    Calculate Values - 1

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.cursoradapter.widget.SimpleCursorAdapter.ViewBinder
 import com.example.tipcalculator.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -38,12 +39,58 @@ class MainActivity : AppCompatActivity() {
        // val calculateButton = findViewById<Button>(R.id.calculateButton)
         // val resetButton = findViewById<Button>(R.id.limpar)
 
+        var tip: String = "0"
+        var total: Float = 0.0f
+
+        binding.tipPercentage0.setOnCheckedChangeListener {_, isChecked ->
+            if (isChecked) {
+                tip = "0"
+            }
+        }
+        binding.tipPercentage10.setOnCheckedChangeListener {_, isChecked ->
+            if (isChecked) {
+                tip = "10"
+            }
+        }
+        binding.tipPercentage20.setOnCheckedChangeListener {_, isChecked ->
+            if (isChecked) {
+                tip = "20"
+            }
+        }
+        binding.tipPercentage25.setOnCheckedChangeListener {_, isChecked ->
+            if (isChecked) {
+                tip = "25"
+            }
+        }
+        binding.tipPercentage30.setOnCheckedChangeListener {_, isChecked ->
+            if (isChecked) {
+                tip = "30"
+            }
+        }
+
 
         binding.calculateButton.setOnClickListener {
-            println(binding.accountInput.text)
-            println(binding.numberOfPeopleInput.text)
-            println(binding.tipPercentage0.isChecked)
+
+                total = calculateValues(binding.accountInput, binding.numberOfPeopleInput, tip).toFloat()
+
+            println(total)
+
         }
+
+        binding.limpar.setOnClickListener {
+            binding.accountInput.text?.clear()
+            binding.numberOfPeopleInput.text?.clear()
+            binding.tipPercentage.clearCheck()
+        }
+
+    }
+
+    private fun calculateValues(accountInput: TextInputEditText, numberOfPeople: TextInputEditText, tip: String): String {
+        if (accountInput.text.isNullOrBlank() || numberOfPeople.text.isNullOrBlank() || tip.isEmpty()) {
+            Snackbar.make(binding.accountInput,"Insert all the values.", Snackbar.LENGTH_LONG).show()
+            return "0.0";
+        }
+        return ((accountInput.text.toString().toDouble() / numberOfPeople.text.toString().toDouble())*(1+tip.toDouble()/100)).toString()
 
     }
 
